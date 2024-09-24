45082905c3b21a146053a3a2b8c071a105f4f3a1
Darpan2311 <darpanvora2311@gmail.com>
2024-09-24 18:15:18 +0530

    added logout

--- app/src/main/java/com/example/examease2/ProfileFragment.java
+++ app/src/main/java/com/example/examease2/ProfileFragment.java
@@ -3,6 +3,7 @@ package com.example.examease2;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,6 +11,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 
@@ -31,9 +37,19 @@ public class ProfileFragment extends Fragment
             public void onClick(View view)
             {
                 FirebaseAuth.getInstance().signOut();
-
+//                GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id))
+//                        .requestEmail().build();
+//                GoogleSignInClient mgoogleclient= GoogleSignIn.getClient(getContext(),gso);
+//                mgoogleclient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        Intent intent = new Intent(getActivity(), Login2.class);
+//                        startActivity(intent);
+//                    }
+//                });
                 Intent intent = new Intent(getActivity(), Login2.class);
                 startActivity(intent);
+
             }
         }
         );
