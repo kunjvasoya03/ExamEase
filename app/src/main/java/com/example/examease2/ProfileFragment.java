b32dcd5c69ddb25d275a049be0a45d70fb01bd2b
Darpan2311 <darpanvora2311@gmail.com>
2024-09-30 23:11:58 +0530

    add ProfileFragment UI

--- app/src/main/java/com/example/examease2/ProfileFragment.java
+++ app/src/main/java/com/example/examease2/ProfileFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -21,7 +22,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment
 {
-    Button logout;
+    LinearLayout logout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,7 +30,7 @@ public class ProfileFragment extends Fragment
     {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        logout = view.findViewById(R.id.logout);
+        logout = view.findViewById(R.id.logoutB);
 
         logout.setOnClickListener(new View.OnClickListener()
         {
